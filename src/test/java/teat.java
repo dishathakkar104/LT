4cee5f6449d02088545715b6921be6ab656f3dba
PALLAV THAKKAR <thakkardisha104@gmail.com>
2023-08-11 14:54:02 -0400

    Assert change

--- src/test/java/teat.java
+++ src/test/java/teat.java
@@ -1,6 +1,5 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
